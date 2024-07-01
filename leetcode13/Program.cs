d8bbc8a24fb58d3dfaa72d20b248caf454a4bf63
NIKKnak <nik88350759@yandex.ru>
2024-07-01 12:10:42 +0300

    leetcode13 end

---
+++ leetcode13/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace leetcode13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanToInt("MCMXCIV"));
+        }
+
+        public static int RomanToInt(string s)
+        {
+            int result = 0;
+
+            s = s.Replace("IV", "IIII");
+            s = s.Replace("IX", "VIIII");
+            s = s.Replace("XL", "XXXX");
+            s = s.Replace("XC", "LXXXX");
+            s = s.Replace("CD", "CCCC");
+            s = s.Replace("CM", "DCCCC");
+
+            char[] arr = s.ToCharArray();
+
+            foreach (var item in arr)
+            {
+                switch (item)
+                {
+                    case 'I': result += 1; break;
+                    case 'V': result += 5; break;
+                    case 'X': result += 10; break;
+                    case 'L': result += 50; break;
+                    case 'C': result += 100; break;
+                    case 'D': result += 500; break;
+                    case 'M': result += 1000; break;
+                }
+            }
+            return result;
+        }
+    }
+}
